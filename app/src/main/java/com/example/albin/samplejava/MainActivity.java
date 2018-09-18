5888ee3abd13d59a145470fe44472870705e79e2
albinalias <albinalias@qburst.com>
2018-09-18 17:47:15 +0530

    java sample

---
+++ app/src/main/java/com/example/albin/samplejava/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.albin.samplejava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private CheckBox check1,check2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onCheckboxClick();
+    }
+    public void onButtonClick(View v){
+
+//        sum of 2 numbers
+        EditText a = findViewById(R.id.editText);
+        EditText b =findViewById(R.id.editText2);
+        TextView c =findViewById(R.id.textView3);
+        int num1 = Integer.parseInt(a.getText().toString());
+        int num2 = Integer.parseInt(b.getText().toString());
+        final int sum = num1 + num2;
+        c.setText(String.valueOf(sum));
+
+//        toast function
+        Toast.makeText(MainActivity.this,String.valueOf(sum),Toast.LENGTH_SHORT).show();
+    }
+
+
+//    checkbox selection program select
+    public void onCheckboxClick(){
+        check1 = findViewById(R.id.checkBox);
+        check2 = findViewById(R.id.checkBox2);
+        Button btn = findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               StringBuilder result = new StringBuilder();
+               if(check1.isChecked()){
+                   result.append("abcd selected");
+               }
+               if (check2.isChecked()){
+                   result.append("\nefgh is selected");
+               }
+                Toast.makeText(MainActivity.this,result.toString(),Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+}
