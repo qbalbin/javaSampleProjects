b0b64e0db5f6ef728ddc0aa07580bb88a55fdf74
albinalias <albinalias@qburst.com>
2018-09-19 10:02:52 +0530

    java sample

--- app/src/main/java/com/example/albin/samplejava/MainActivity.java
+++ app/src/main/java/com/example/albin/samplejava/MainActivity.java
@@ -6,17 +6,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox check1,check2;
+    private RadioButton radi1;
+    private Button bttn;
+    private RadioGroup rgp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onCheckboxClick();
+        radioButtonClickListener();
     }
     public void onButtonClick(View v){
 
@@ -34,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    checkbox selection program select
+    //    checkbox selection program select
     public void onCheckboxClick(){
         check1 = findViewById(R.id.checkBox);
         check2 = findViewById(R.id.checkBox2);
@@ -43,17 +49,31 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               StringBuilder result = new StringBuilder();
-               if(check1.isChecked()){
-                   result.append("abcd selected");
-               }
-               if (check2.isChecked()){
-                   result.append("\nefgh is selected");
-               }
+                StringBuilder result = new StringBuilder();
+                if(check1.isChecked()){
+                    result.append("abcd selected");
+                }
+                if (check2.isChecked()){
+                    result.append("\nefgh is selected");
+                }
                 Toast.makeText(MainActivity.this,result.toString(),Toast.LENGTH_SHORT).show();
             }
 
         });
     }
 
+    //    radiobutton selection programme
+    public void radioButtonClickListener(){
+        bttn = findViewById(R.id.button3);
+        rgp = findViewById(R.id.radiogroup);
+
+        bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selected_id = rgp.getCheckedRadioButtonId();
+                radi1 = findViewById(selected_id);
+                Toast.makeText(MainActivity.this,radi1.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
